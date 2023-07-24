477735132e7c118cc9ae7c39142ab84ae5533389
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-24 09:22:18 -0600

    feat: add fetch call to api file

--- src/components/apiCalls.js
+++ src/components/apiCalls.js
@@ -1,3 +1,11 @@
-function fetchTricks() {
- 
+async function fetchTrick() {
+  const response = await fetch('http://localhost:3001/api/v1/tricks')
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const data = await response.json()
+  return data
 }
+
+
+export default fetchTrick
