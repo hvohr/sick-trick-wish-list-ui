1226690db38ad0464999ddc8ec41dee267f86416
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-24 09:25:56 -0600

    feat: set up fetch function on main page and implement useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import React from 'react';
-
-
+import fetchTrick from '../apiCalls'
+import { useEffect } from 'react'
 
 function App() {
 
+function showTricks() {
+  return (
+    fetchTrick().then(
+      data => console.log(data)
+    )
+  )
+}
 
-
+useEffect(() => {
+  showTricks()
+}, [])
   
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
