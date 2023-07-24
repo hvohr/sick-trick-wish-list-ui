4787658f4f47d720bebebdc9b1a44e5149616ed7
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-24 09:49:38 -0600

    fix: add stance to tricks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ function App() {
   }
 
   useEffect(() => {
+    console.log(tricks)
     showTricks()
   }, [])
 
--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -3,7 +3,7 @@ import './TrickCard.css'
 function TrickCard(props) {
   return (
     <section className='trick-card-container'>
-      <h2 className='trick-name'>{props.name}</h2>
+      <h2 className='trick-name'>{props.stance} {props.name}</h2>
       <h2 className='trick-name'>{props.obstacle}</h2>
       <a href={props.link} className='trick-name'>{props.link}</a>
     </section>
