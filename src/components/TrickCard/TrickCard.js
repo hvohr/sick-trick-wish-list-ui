aa49bcccfa92b730301d004a7ef1797eb96b7159
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-24 09:47:42 -0600

    feat: add more styling to trick card

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -3,9 +3,9 @@ import './TrickCard.css'
 function TrickCard(props) {
   return (
     <section className='trick-card-container'>
-      <h2 clasName='trick-name'>{props.name}</h2>
-      <h2 clasName='trick-name'>{props.obstacle}</h2>
-      <h2 clasName='trick-name'>{props.link}</h2>
+      <h2 className='trick-name'>{props.name}</h2>
+      <h2 className='trick-name'>{props.obstacle}</h2>
+      <a href={props.link} className='trick-name'>{props.link}</a>
     </section>
   )
 }
